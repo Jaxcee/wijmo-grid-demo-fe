3a4c7cf85c9f89017387737e457086099d4365f4
Jagadesh J <jagadesh112002@gmail.com>
2024-06-03 13:38:46 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridComponent } from './components/grid/grid.component';
+import * as wjGrid from '@mescius/wijmo.angular2.grid';
+import { WjGridFilterModule } from '@mescius/wijmo.angular2.grid.filter';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    wjGrid.WjGridModule,
+    WjGridFilterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+enableProdMode();
+platformBrowserDynamic().bootstrapModule(AppModule);
